dbd29f897b6ae00fd4065af0de9c5622a4f47876
NaviD Kian <n.soleimani@haierasa.ir>
2019-12-15 01:01:30 +0330

    13980923

--- General/Global.asax.cs
+++ General/Global.asax.cs
@@ -1,7 +1,9 @@
 ﻿using General;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -17,6 +19,16 @@ namespace General
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            var persianCulture = new PersianCulture();
+            persianCulture.DateTimeFormat.ShortDatePattern = "yyyy/MM/dd";
+            persianCulture.DateTimeFormat.LongDatePattern = "dddd d MMMM yyyy";
+            persianCulture.DateTimeFormat.AMDesignator = "صبح";
+            persianCulture.DateTimeFormat.PMDesignator = "عصر";
+            Thread.CurrentThread.CurrentCulture = persianCulture;
+            Thread.CurrentThread.CurrentUICulture = persianCulture;
         }
     }
 }
